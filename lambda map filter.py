9be41af7f0313928f69610a64fb66711b16ae1e9
ajmal4414 <ajmalsckl@gmail.com>
2023-01-29 21:54:50 +0530

    first commit

---
+++ lambda map filter.py
@@ -0,0 +1,21 @@
+# lambda function
+# lambda is a keyword in python used to define functions.more specifically anonymous functions and such functions
+# are known as lanbda functions
+# convert string to uppercase and reverse it
+# str1="helloworld"
+# rev_upper=lambda string:string.upper()[::-1]
+# print(rev_upper(str1))
+
+# map
+# the map() function returns a map object(which is an iterator) of the results after applying the given function
+# to each item of a given iterable(list,tuple etc)
+# return double of n
+# def add(n):
+#     return n+n
+# num=(1,2,3,4)
+# res=map(add,num)
+# print(list(res))
+
+#filter
+# the filter() method filters the given sequence with the help of a function that tests each element in the
+# sequence to be true or not
---
+++ main.py
@@ -0,0 +1,18 @@
+# write a programme to create a lambda function that adds 15 to a given number passed in as an argument
+# also create a lambda function that multiplies argument x with argument y and print the result
+
+# a=lambda x: x +15
+# print(a(10))
+# b=lambda x,y: x*y
+# print(b(12,4))
+#
+# write a programme sort a list of tuples using lambda
+
+# subject_mark=[('english',88),('science',90),('maths',97),('socialscience',82)]
+# print('original list of mark:')
+# print(subject_mark)
+# subject_mark.sort(key=lambda x:x[1])
+# print("sorting list of marks")
+# print(subject_mark)
+#
+
